11e99b2a089ae706fa8ce7054f24b9550698973e
Robert Wilson <youmufan@gmail.com>
2018-04-11 19:25:44 -0400

    added conditional rendering and logic that helps dictate game flow

--- src/App.js
+++ src/App.js
@@ -18,44 +18,113 @@ class App extends Component {
         cardCount: 0,
         toMax: 21,
         isBust: false,
-        isTurn: false,
+        isTurn: true,
       },
     }
     this.dealCard = this.dealCard.bind(this);
-    this.checkResults = this.checkResults.bind(this);
   }
 
-  checkResults(target) {
-    const tempState = this.state[target];
-    tempState.toMax
-    if(tempState.toMax === 0) {
-      console.log('blackjack');
-    } else if (tempState.toMax < 0) { // negative value
-      this.setState({
-        ...tempState,
-        isBust: true,
-      })
-    }
-    console.log('test',tempState.toMax);
+  reset() {
+    this.setState({
+      dealer: {
+        total: 0,
+        cardCount: 0,
+        toMax: 21,
+        isBust: false,
+        isTurn: false,
+      },
+      player: { 
+        total: 0,
+        cardCount: 0,
+        toMax: 21,
+        isBust: false,
+        isTurn: true,
+      },
+    })
+  }
+
+  endTurn() {
+    const tempPlayer = this.state.player;
+    const tempDealer = this.state.dealer;
+    
+    this.setState({
+      player: {
+        ...tempPlayer,
+        isTurn: false,
+      },
+      dealer: {
+        ...tempDealer,
+        isTurn: true,
+      },
+    })
+
+    console.log(this.state.player)
   }
 
   dealCard(target) {
+
     let cardValue = (Math.floor(Math.random() * 10)) + 1;
     const tempState = this.state[target];
-    this.setState({
-      [target]: {
-        ...tempState,
-        cardCount: tempState.cardCount + 1,
-        total: tempState.total + cardValue,
-        toMax: tempState.toMax - cardValue,
+
+    const checkResults = (target, cardValue) => {
+      if(tempState.toMax < 5 && target === "dealer") {
+        console.log('dealer end');
+        this.setState({
+          dealer: {
+            ...tempState,
+            isTurn: false,
+          },
+        })
+        return;
       }
-    });
-    console.log(cardValue);
-    this.checkResults(target);
+      let tempValue = tempState.toMax - cardValue;
+      if(tempValue === 0) {
+        console.log('blackjack');
+      } else if (tempValue < 0) { // negative value
+        this.setState({
+          [target]: {
+            ...tempState,
+            cardCount: tempState.cardCount + 1,
+            total: tempState.total + cardValue,
+            toMax: tempState.toMax - cardValue,
+            isBust: true,
+            isTurn: false,
+          }
+        })
+      } else {     
+        this.setState({
+          [target]: {
+            ...tempState,
+            cardCount: tempState.cardCount + 1,
+            total: tempState.total + cardValue,
+            toMax: tempState.toMax - cardValue,
+          }
+        });
+      }  
+    }
+    checkResults(target, cardValue);
   }
 
   render() {
-    return (
+    if(this.state.player.isTurn) {
+      return (
+        <div>
+          <h1>Cookie Jar</h1>
+          <div className="flex-container-h">
+            <div>Dealer</div>
+            <CardCounter value={this.state.dealer.total} />
+          </div>
+          <div className="flex-container-h">
+            <div>You</div>
+            <CardCounter value={this.state.player.total} />
+          </div>
+          <div className="flex-container-h">
+            <button onClick={() => this.dealCard('player')}>Hit!</button>
+            <button onClick={() => this.endTurn()}>Stand</button>
+          </div>
+        </div> );
+    } else if(this.state.dealer.isTurn) {
+      return (
       <div>
         <h1>Cookie Jar</h1>
         <div className="flex-container-h">
@@ -67,24 +136,26 @@ class App extends Component {
           <CardCounter value={this.state.player.total} />
         </div>
         <div className="flex-container-h">
-          <button onClick={() => this.dealCard('player')}>Hit!</button>
-          <button onClick={() => console.log(this.state.player)}>Stand</button>
           <button onClick={() => this.dealCard('dealer')}>Hit Dealer</button>
         </div>
-      </div>
-    );
-  }
-  componentDidMount() {
-    let tempValue = (Math.floor(Math.random() * 10)) + 1;
-    this.setState({
-      dealer: {
-        total: tempValue,
-        cardCount: 1,
-        toMax: 21 - tempValue,
-        isBust: false,
-        isTurn: false,
-      },
-    })
+      </div> );
+    } else {
+      return (
+      <div>
+        <h1>Cookie Jar</h1>
+        <div className="flex-container-h">
+          <div>Dealer</div>
+          <CardCounter value={this.state.dealer.total} />
+        </div>
+        <div className="flex-container-h">
+          <div>You</div>
+          <CardCounter value={this.state.player.total} />
+        </div>
+        <div className="flex-container-h">
+          <button onClick={() => this.reset()}>Restart Game</button>
+        </div>
+      </div> );
+    }
   }
 }
 
